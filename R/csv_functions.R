b4cf1747fbcfa27dfdb2b21a24a7dea918ff0e9e
Gregoire Virepinte <gregoirevirepinte@MacBook-Air-de-Gregoire-2.local>
2017-10-16 13:38:47 +0200

    First test

---
+++ R/csv_functions.R
@@ -0,0 +1,47 @@
+#' save_as_csv
+#'
+#' @param dataset A dataset, basically
+#'
+#' @return The full path of the csv where this dataset has been saved
+#' @import tidyverse
+#' @export
+#'
+#' @examples
+
+save_as_csv <- function(dataset, filename) {
+
+  assert_that(grepl(".csv", filename))
+  assert_that(is.data.frame(iris))
+  assert_that(dir.exists(dirname(filename)))
+
+  dataset %>% write.csv2(filename)
+  return(normalizePath(filename))
+
+}
+
+
+#' My csv reader
+#'
+#' @param folderpath The path of the folder
+#'
+#' @return A list of all the csv files contained in the folder
+#' @import(tidyr)
+#' @import(readxl)
+#' @export
+#'
+#' @examples
+
+my_csv_reader <- function(folderpath) {
+
+  assert_that(dir.exists(folderpath))
+
+  csv_list = list()
+  for(item in folderpath %>% list.files(pattern = ".*csv$", full.names = TRUE)) {
+    csv_list[[length(csv_list)+1]] <- read.csv2(item)
+  }
+  return(csv_list)
+
+
+}
+
+
---
+++ R/name_functions.R
@@ -0,0 +1,64 @@
+library(prenoms)
+library(ggplot2)
+library(dplyr)
+library(dygraphs)
+library(tidyr)
+library(assertthat)
+data(prenoms)
+
+#' Draw a name
+#'
+#' @param the_name Name searched
+#' @param the_sex Sex searched
+#'
+#' @return Returns a graph with the number of times the name was given every year since 1900
+#' @import(prenoms)
+#' @import(ggplot2)
+#' @import(dplyr)
+#' @import(dygraphs)
+#' @import(tidyr)
+#' @export
+#'
+#' @examples
+#' draw_a_name("Vincent", "M")
+
+draw_a_name <- function(the_name, the_sex) {
+
+  assert_that(is.character(the_name), the_sex %in% c("M", "F"))
+  assert_that(the_name %in% (prenoms %>% filter(sex == the_sex))[["name"]])
+
+  return(prenoms %>% filter(name == the_name, sex == the_sex) %>%
+           group_by(year) %>%
+           summarise(n = sum(n)) %>%
+           ggplot(aes(x = year, y = n)) +
+           geom_line())
+}
+
+#' Draw names
+#'
+#' @param the_names Names searched (vector form)
+#'
+#' @return Returns a graph with the number of times each name was given every year since 1900
+#' @import(prenoms)
+#' @import(ggplot2)
+#' @import(dplyr)
+#' @import(dygraphs)
+#' @import(tidyr)
+#' @export
+#'
+#' @examples
+#' draw_names(c("Vincent", "Diane"))
+
+draw_names <- function(the_names) {
+
+  for(name in the_names) {
+    assert_that(name %in% prenoms$name)
+  }
+
+  return(prenoms %>% filter (name %in% the_names) %>%
+           group_by(year, name) %>%
+           summarise(n = sum(n)) %>%
+           ggplot(aes(x = year, y = n, color = name)) +
+           geom_line())
+
+}
